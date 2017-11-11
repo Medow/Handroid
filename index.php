c7314d22442f9de10e43358541d5c16ebaf0bc95
Medow <baharmahir@yahoo.com>
2017-11-11 11:29:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<head>
+  <meta name="description"  content="Handroid - The Best Motorcycle Gloves in the world" />
+
+<meta name="keywords"  content="handroid, Best Motorcycle Gloves" />
+</head>
+
+<b>Handroid - The Best Motorcycle Gloves in the world</b>
